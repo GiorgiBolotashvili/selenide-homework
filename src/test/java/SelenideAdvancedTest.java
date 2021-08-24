a3c398bb59987e90a3285ed6e0c58aeab3ad3a1c
Giorgi <69397245+Bolota85@users.noreply.github.com>
2021-08-24 22:11:57 +0400

    step 2

--- src/test/java/SelenideAdvancedTest.java
+++ src/test/java/SelenideAdvancedTest.java
@@ -1,15 +1,18 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-import static com.codeborne.selenide.Selenide.*;
 
 import java.util.ArrayList;
 
-public class SelenideBasics2Test {
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideAdvancedTest {
     @Test
-    public void softAssert() throws InterruptedException {
+    public void advanced() throws InterruptedException {
         open("https://demoqa.com/books ");
         SoftAssert softAssert = new SoftAssert();
         ArrayList<WebElement> javaScriptArray = new ArrayList<>();
@@ -26,28 +29,21 @@ public class SelenideBasics2Test {
             }
         }
 //        Check that size of returned list equals to 10
-        softAssert.assertEquals(javaScriptArray.size(),10, "array size is not 10");
-//        Find that each books images are not empty
-        $("div.rt-tbody").findAll("div > div > div > img").stream().forEach(y -> {
-            softAssert.assertFalse(y.getAttribute("src").isEmpty());
-        });
+        softAssert.assertEquals(javaScriptArray.size(), 10, "array size is not 10");
+//      Check that first match row's title equals to 'Learning JavaScript Design Patterns'
+        for (int i = 0; i < javaScriptArray.size(); i++) {
+            if (i == 0) {
+                softAssert.assertEquals(javaScriptArray.get(i).getText(), "Learning JavaScript Design Patterns");
+                System.out.println("First row title is: " + javaScriptArray.get(i).getText());
+            }
+
+        }
+
+
+        //     $$("//*[@id=\"app\"]/div/div/div[2]/div[2]/div[1]/div[2]/div[1]/div[2]/div[1]").filter(Condition.exist("O'Reilly Media")).filter()
+//         $$("div.rt-tbody").filter("//div[2]/div/div[4]".contains("O'Reilly Media"))
 
         softAssert.assertAll();
     }
+
 }
-
-
-
-
-
-
-
-
-
-
-/*
-    ElementsCollection bookImages = $("div.rt-tbody").findAll("div > div > div > img");
-            System.out.println(bookImages.size());
-        for (WebElement item : bookImages) {
-                System.out.println(item.getAttribute("src").isEmpty());
-                }*/
