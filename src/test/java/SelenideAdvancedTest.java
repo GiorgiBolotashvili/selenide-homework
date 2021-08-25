d55612f44a00e0d5ea09991e86b0982ed83085df
Giorgi <69397245+Bolota85@users.noreply.github.com>
2021-08-26 01:34:30 +0400

    finish homework

--- src/test/java/SelenideAdvancedTest.java
+++ src/test/java/SelenideAdvancedTest.java
@@ -1,49 +1,77 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-import java.util.ArrayList;
+import java.io.FileNotFoundException;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SelenideAdvancedTest {
     @Test
-    public void advanced() throws InterruptedException {
+    public void advanced() throws FileNotFoundException {
+        // System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+        // Configuration.browser = "chrome";
+        Configuration.reportsFolder = "\\src\\main\\resources\\Reports";
+        Configuration.savePageSource = false;
         open("https://demoqa.com/books ");
         SoftAssert softAssert = new SoftAssert();
-        ArrayList<WebElement> javaScriptArray = new ArrayList<>();
 //        Find all books with publisher 'O'Reilly Media' containing title 'Javascript'
-        ElementsCollection divList = $("div.rt-tbody").findAll("div[class='rt-tr-group']");
-        for (WebElement el : divList) {
-            WebElement item = $(el.findElement(By.cssSelector("div > div:nth-child(4)")));
-            if (item.getAttribute("innerText").equals("O'Reilly Media")) {
-                WebElement result = $(el.findElement(By.cssSelector("div > div:nth-child(2)")));
-                if (result.getText().contains("JavaScript")) {
-                    javaScriptArray.add(result);
-                    System.out.println(result.getText());
-                }
-            }
+        ElementsCollection list = $("div.rt-tbody")
+                .findAll(By.xpath("//span[contains (@id,\"JavaScript\")]/../../../div[4][text()=\"O'Reilly Media\"]/../.."));
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("Book " + (i + 1) + " >> " + list.get(i).text());
+            System.out.println("----");
         }
+        System.out.println();
+
 //        Check that size of returned list equals to 10
-        softAssert.assertEquals(javaScriptArray.size(), 10, "array size is not 10");
+        softAssert.assertEquals(list.size(), 10, "array size is not 10");
+
 //      Check that first match row's title equals to 'Learning JavaScript Design Patterns'
-        for (int i = 0; i < javaScriptArray.size(); i++) {
+        for (int i = 0; i < list.size(); i++) {
             if (i == 0) {
-                softAssert.assertEquals(javaScriptArray.get(i).getText(), "Learning JavaScript Design Patterns");
-                System.out.println("First row title is: " + javaScriptArray.get(i).getText());
+                softAssert.assertEquals(list.get(i).find("span").getText(), "Learning JavaScript Design Patterns");
+                System.out.println("First row title is: " + list.get(i).find("span").getText());
             }
-
         }
+        System.out.println("-----------------------------------");
 
-
-        //     $$("//*[@id=\"app\"]/div/div/div[2]/div[2]/div[1]/div[2]/div[1]/div[2]/div[1]").filter(Condition.exist("O'Reilly Media")).filter()
-//         $$("div.rt-tbody").filter("//div[2]/div/div[4]".contains("O'Reilly Media"))
+//        Using stream() click on all matching row's title
+        list.stream().forEach(y -> {
+            y.find(".mr-2 > a").click();
+            System.out.println(WebDriverRunner.url());
+            back();
+        });
 
         softAssert.assertAll();
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        ElementsCollection list = $("div.rt-tbody").findAll("span[id*='JavaScript'] > div");
